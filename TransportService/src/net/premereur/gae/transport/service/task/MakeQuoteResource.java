37aabca453adae36a907143f09eb193d018c4e51
Geert Premereur <geert.premereur@gmail.com>
2010-01-30 17:36:13 +0100

    No need to return quotes XML in task

--- TransportService/src/net/premereur/gae/transport/service/task/MakeQuoteResource.java
+++ TransportService/src/net/premereur/gae/transport/service/task/MakeQuoteResource.java
@@ -5,12 +5,12 @@ import java.util.logging.Logger;
 
 import net.premereur.gae.transport.domain.QuoteRequest;
 import net.premereur.gae.transport.domain.QuoteRequestRepository;
-import net.premereur.gae.transport.domain.Quotes;
 import net.premereur.gae.transport.service.common.ResourceBase;
 
+import org.restlet.data.MediaType;
 import org.restlet.data.Status;
-import org.restlet.ext.jaxb.JaxbRepresentation;
 import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Post;
 
 import com.google.inject.Inject;
@@ -36,15 +36,16 @@ public class MakeQuoteResource extends ResourceBase {
 	public Representation computeOffers(Representation entry) {
 		logger.entering(MakeQuoteResource.class.getName(), "computeOffers", entry);
 		Long quoteRequestId = getRequestId();
-		QuoteRequest quoteRequest;
 		try {
-			quoteRequest = getRepository().findByKey(quoteRequestId);
+			final QuoteRequest quoteRequest = getRepository().findByKey(quoteRequestId);
+			quoteRequest.getQuotes();
+			setStatus(Status.SUCCESS_OK);
+			return new StringRepresentation("Item created", MediaType.TEXT_PLAIN);
 		} catch (Exception e) {
 			logger.log(Level.INFO, "Couldn't retrieve quoteRequest with key " + quoteRequestId, e);
 			setExisting(false);
 			setStatus(Status.CLIENT_ERROR_NOT_FOUND);
 			return null;
 		}
-		return new JaxbRepresentation<Quotes>(quoteRequest.getQuotes());
 	}
 }
